77e1824a2d144c136f75eb574e62062af437e373
Jun FURUSE <jun.furuse@sc.com>
2015-02-02 19:27:55 +0800

    moving to ppx_test

--- xtest.mli
+++
@@ -1,9 +0,0 @@
-type t
-
-val arg_specs : (string * Arg.spec * string) list
-
-val fun_ : (unit -> unit) -> t
-val label : string -> t -> t
-val list : t list -> t
-
-val run : bool (** default_go *) -> t -> unit
